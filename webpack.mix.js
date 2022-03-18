f443d68da622c57ffc367dc823dc7dc072420266
Loek van der Linde <lind0096@hz.nl>
2022-03-18 10:38:25 +0100

    Update dependencies and their references

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,11 +1,26 @@
 let mix = require('laravel-mix');
 
 mix
+    // Images
     .copy('src/img', 'dist/img')
-    .copy('src/index.html', 'dist/index.html')
-    .copy('node_modules/@fortawesome/fontawesome-free/webfonts/fa-brands-*', 'dist/webfonts')
 
+    // HTML
+    .copy('src/index.html', 'dist/index.html')
+    .copy('src/tour.html', 'dist/tour.html')
+
+    // Downloads
+    .copy('src/presskit', 'dist/presskit')
+
+    // Fonts
+    .copy('node_modules/@fortawesome/fontawesome-free/webfonts/fa-brands-*', 'dist/webfonts')
+    .copy('node_modules/@fortawesome/fontawesome-free/webfonts/fa-solid-*', 'dist/webfonts')
+
+    // JavaScript
     .js('src/js/app.js', 'js')
+
+    // CSS
     .sass('src/sass/app.scss', 'css').options({ processCssUrls: false })
+
+    // Dist folder is public folder
     .setPublicPath('dist')
 ;
