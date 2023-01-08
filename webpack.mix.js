293cebdac57dd5d550a5b4d64ec6e9bf44a372b7
Lucky-Loek <lind0096@hz.nl>
2023-01-08 14:59:40 +0100

    Update dependencies

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,15 +1,9 @@
 let mix = require('laravel-mix');
 
 mix
-    // Images
-    .copy('src/img', 'dist/img')
-
     // HTML
     .copy('src/index.html', 'dist/index.html')
     .copy('src/tour.html', 'dist/tour.html')
-
-    // Downloads
-    .copy('src/presskit', 'dist/presskit')
 
     // Fonts
     .copy('node_modules/@fortawesome/fontawesome-free/webfonts/fa-brands-*', 'dist/webfonts')
