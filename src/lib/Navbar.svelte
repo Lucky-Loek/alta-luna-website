c613e382190e59dec056baeea5ca3609e5d38d45
Lucky-Loek <lind0096@hz.nl>
2023-01-13 11:38:10 +0100

    Fix bug where navbar would not fold open on small screens

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -1,16 +1,24 @@
+<script lang="ts">
+	let showBurgerMenu = false;
+</script>
+
 <nav class="navbar is-dark">
     <div class="navbar-brand">
         <a class="navbar-item" href="https://altalunaband.com">
             <img src="/img/Logowit.png" alt="Alta Luna logo" width="40" height="40">
         </a>
-        <div class="navbar-burger burger" data-target="navbar">
+        <div
+					class="navbar-burger burger" class:is-active="{showBurgerMenu}"
+					on:click="{() => showBurgerMenu = !showBurgerMenu}">
             <span></span>
             <span></span>
             <span></span>
         </div>
     </div>
 
-    <div id="navbar" class="navbar-menu">
+    <div
+			class="navbar-menu" class:is-active="{showBurgerMenu}"
+			on:click="{() => showBurgerMenu = !showBurgerMenu}">
         <div class="navbar-start">
             <a class="navbar-item plume" href="#about">
                 About
